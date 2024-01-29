9a9092c500ed48d6908335e5c25c4c3e62524f2e
등딱지빌런 <96368476+inhopp@users.noreply.github.com>
2024-01-29 21:44:48 +0900

    [Gold V] Title: 퇴사 2, Time: 212 ms, Memory: 7880 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <tuple>
+#include <map>
+using namespace std;
+
+
+long long N;
+int D[1500051];
+int T, P;
+
+int main(void)
+{
+	// freopen("input.txt", "r", stdin);
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+
+	int mx = 0;
+	int idx = 1;
+
+	while (N--)
+	{
+		cin >> T >> P;
+
+		D[idx - 1 + T] = max(D[idx - 1 + T], mx + P);
+		mx = max(mx, D[idx]);
+		idx++;
+	}
+
+	cout << mx << "\n";
+}
