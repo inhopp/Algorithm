01dc83042d6d8686d778173fac6423809e9a5180
등딱지빌런 <96368476+inhopp@users.noreply.github.com>
2024-06-10 12:24:06 +0900

    [Gold V] Title: 토마토, Time: 88 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,72 +1,77 @@
-#define _CRT_SECURE_NO_DEPRECATE
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <string>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+
 using namespace std;
 
 #define X first
 #define Y second
 
-int A[1000][1000];
-int V[1000][1000];
+int A[1005][1005];
+int V[1005][1005];
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int total_size;
+int cur_size;
+int max_cnt;
+
+queue<pair<int, int>> Q;
 
 int main(void)
 {
-	// freopen("input.txt", "r", stdin);
+    // freopen("input.txt", "r", stdin);
 
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	int m, n;
-	cin >> m >> n;
+    int N, M;
+    cin >> M >> N;
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			cin >> A[i][j];
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++) {
+            cin >> A[i][j];
 
-	int mx = 0;
-	int dx[4] = { -1, 0, 1, 0 };
-	int dy[4] = { 0, -1, 0, 1 };
+            if (A[i][j] == -1)
+                continue;
 
-	queue<pair<int, int>> Q;
+            if (A[i][j] == 1) {
+                Q.push({i, j});
+                cur_size++;
+            }
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			if (A[i][j] == 1)
-				Q.push({ i, j });
+            total_size++;
+        }
 
-	while (!Q.empty())
-	{
-		pair<int, int> cur = Q.front();
-		Q.pop();
+    while(!Q.empty()) {
+        auto cur = Q.front();
+        Q.pop();
 
-		for (int dir = 0; dir < 4; dir++)
-		{
-			int nx = cur.X + dx[dir];
-			int ny = cur.Y + dy[dir];
+        for (int dir = 0; dir < 4; dir++) {
+            int x = cur.X + dx[dir];
+            int y = cur.Y + dy[dir];
 
-			if (nx >= 0 and nx < n and ny >= 0 and ny < m)
-			{
-				if (A[nx][ny] == 0 and V[nx][ny] == 0)
-				{
-					Q.push({ nx,ny });
-					A[nx][ny] = 1;
-					V[nx][ny] = V[cur.X][cur.Y] + 1;
-					mx = max(mx, V[nx][ny]);
-				}
-			}
-		}
-	}
-
-
-	for(int i=0; i<n; i++)
-		for(int j=0; j<m; j++)
-			if (A[i][j] == 0)
-			{
-				cout << -1;
-				return 0;
-			}
-
-	cout << mx;
+            if (x<0 || x>=N || y<0 || y>=M)
+                continue;
+            
+            if (A[x][y] == 0) {
+                Q.push({x, y});
+                A[x][y] = 1;
+                V[x][y] = V[cur.X][cur.Y] + 1;
+                cur_size++;
+                max_cnt = max(max_cnt, V[x][y]);
+            }
+        }
+    }
+    
+    if (cur_size == total_size)
+        cout << max_cnt << endl;
+    
+    else
+        cout << -1 << endl;
 }
