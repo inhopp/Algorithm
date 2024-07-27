c87abadceaf120b73cc62ed4ff88491829cfbc9b
등딱지빌런 <96368476+inhopp@users.noreply.github.com>
2024-07-28 04:04:03 +0900

    [Silver I] Title: 트럭, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.cc
+++ 백준/Silver/13335. 트럭/트럭.cc
@@ -5,13 +5,15 @@
 #include <algorithm>
 #include <string>
 #include <tuple>
+#include <map>
+#include <math.h>
 using namespace std;
 
-int n, w, L;
-deque<int> T;
-deque<int> B;
-deque<int> A;
-int t, cnt;
+queue<int> T;
+queue<int> B;
+
+int N, W, L;
+int res, onBridge, sum, total;
 
 int main(void)
 {
@@ -20,43 +22,37 @@ int main(void)
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> n >> w >> L;
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> t;
-		T.push_back(t);
+	cin >> N >> W >> L;
+	int v;
+	for (int i = 0; i < N; i++) {
+		cin >> v;
+		total += v;
+		T.push(v);
 	}
 
-	for (int i = 0; i < w; i++)
-		B.push_back(0);
+	for (int i = 0; i < W; i++)
+		B.push(0);
 
-	while (A.size() < n)
-	{
-		cnt++;
+	while (sum < total) {
+		res++;
 
-		if (B.front() > 0) A.push_back(B.front());
-		B.pop_front();
+		sum += B.front();
+		onBridge -= B.front();
+		B.pop();
 
-		if (T.empty()) B.push_back(0);
+		if (T.empty())
+			continue;
 
-		else
-		{
-			int sum = 0;
-			for (int i = 0; i < w-1; i++)
-				sum += B[i];
+		if (onBridge + T.front() > L) {
+			B.push(0);
+		}
 
-			if (sum + T.front() > L)
-				B.push_back(0);
-
-			else
-			{
-				B.push_back(T.front());
-				T.pop_front();
-			}
-
+		else {
+			onBridge += T.front();
+			B.push(T.front());
+			T.pop();
 		}
 	}
 
-	cout << cnt << '\n';
+	cout << res << endl;
 }
