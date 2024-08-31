4af6c3a5c5e1b3e844b4dccae6df8ad503b2d4d9
등딱지빌런 <96368476+inhopp@users.noreply.github.com>
2024-08-31 23:37:57 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -6,7 +6,11 @@
 #include <string>
 #include <tuple>
 #include <map>
+#include <math.h>
+
 using namespace std;
+
+long long A[105];
 
 int main(void)
 {
@@ -15,34 +19,20 @@ int main(void)
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
+	A[1] = 1;
+	A[2] = 1;
+	A[3] = 1;
+	A[4] = 2;
+	A[5] = 2;
+
+	for (int i = 6; i <= 100; i++)
+		A[i] = A[i - 1] + A[i - 5];
+
 	int T, N;
 	cin >> T;
 
-	long long B[102];
-	B[1] = 1;
-	B[2] = 1;
-	B[3] = 1;
-
-	deque<long long> Q;
-
-	while (T--)
-	{
+	while (T--) {
 		cin >> N;
-		Q.push_back(N);
-	}
-
-	int mx=0;
-	for (int q : Q)
-		mx = max(mx, q);
-
-	for (int i = 4; i <= mx; i++)
-		B[i] = B[i - 2] + B[i - 3];
-
-	while (!Q.empty())
-	{
-		int idx = Q.front();
-		Q.pop_front();
-
-		cout << B[idx] << "\n";
+		cout << A[N] << endl;
 	}
 }
