836aa9499349a71fce0b48ab8626ec746391f0d6
등딱지빌런 <96368476+inhopp@users.noreply.github.com>
2024-09-20 15:06:05 +0900

    [Silver II] Title: 제곱수의 합, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -6,27 +6,31 @@
 #include <string>
 #include <tuple>
 #include <map>
+#include <math.h>
+
 using namespace std;
 
-int N;
-int D[100001];
+int D[100005];
 
 int main(void)
 {
 	// freopen("input.txt", "r", stdin);
-
+	
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
+	int N;
 	cin >> N;
 
-	for (int i = 1; i <= N; i++)
-	{
+	for (int i = 1; i <= N; i++) {
 		D[i] = i;
+		
+		for (int j = 0; j <= i; j++) {
+			if (j * j > i) break;
 
-		for (int j = 1; j * j <= i; j++)
 			D[i] = min(D[i], D[i - j * j] + 1);
+		}
 	}
 
-	cout << D[N] << "\n";
+	cout << D[N] << endl;
 }
