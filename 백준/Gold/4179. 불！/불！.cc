568c3819c414d309a1513991a6b2852d3d921924
등딱지빌런 <96368476+inhopp@users.noreply.github.com>
2024-10-31 12:24:56 +0900

    [Gold III] Title: 불!, Time: 32 ms, Memory: 11032 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,68 +1,103 @@
-// Authored by : BaaaaaaaaaaarkingDog
-// Co-authored by : -
-// http://boj.kr/aed4ec552d844acd8853111179d5775d
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+
 using namespace std;
+
 #define X first
 #define Y second
-string board[1002];
-int dist1[1002][1002]; // 불의 전파 시간
-int dist2[1002][1002]; // 지훈이의 이동 시간
-int n, m;
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> m;
-  for(int i = 0; i < n; i++){
-    fill(dist1[i], dist1[i]+m, -1);
-    fill(dist2[i], dist2[i]+m, -1);    
-  }
-  for(int i = 0; i < n; i++)
-    cin >> board[i];  
-  queue<pair<int,int> > Q1;
-  queue<pair<int,int> > Q2;
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-      if(board[i][j] == 'F'){
-        Q1.push({i,j});
-        dist1[i][j] = 0;        
-      }
-      if(board[i][j] == 'J'){
-        Q2.push({i,j});
-        dist2[i][j] = 0;
-      }
-    }
-  }
-  // 불에 대한 BFS
-  while(!Q1.empty()){
-    auto cur = Q1.front(); Q1.pop();
-    for(int dir = 0; dir < 4; dir++){
-      int nx = cur.X + dx[dir];
-      int ny = cur.Y + dy[dir];
-      if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-      if(dist1[nx][ny] >= 0 || board[nx][ny] == '#') continue; 
-      dist1[nx][ny] = dist1[cur.X][cur.Y]+1;
-      Q1.push({nx,ny});
-    }
-  }
 
-  // 지훈이에 대한 BFS
-  while(!Q2.empty()){
-    auto cur = Q2.front(); Q2.pop();
-    for(int dir = 0; dir < 4; dir++){
-      int nx = cur.X + dx[dir];
-      int ny = cur.Y + dy[dir];
-      if(nx < 0 || nx >= n || ny < 0 || ny >= m){ // 범위를 벗어났다는 것은 탈출에 성공했다는 의미. 큐에 거리 순으로 들어가므로 최초에 탈출한 시간을 출력하면 됨.
-        cout << dist2[cur.X][cur.Y]+1; 
-        return 0;
-      }
-      if(dist2[nx][ny] >= 0 || board[nx][ny] == '#') continue;
-      if(dist1[nx][ny] != -1 && dist1[nx][ny] <= dist2[cur.X][cur.Y]+1) continue; // 불의 전파 시간을 조건에 추가
-      dist2[nx][ny] = dist2[cur.X][cur.Y]+1;
-      Q2.push({nx,ny});
+char A[1005][1005];
+int V1[1005][1005];
+int V2[1005][1005];
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+queue<pair<int, int>> Q1;
+queue<pair<int, int>> Q2;
+
+int main(void)
+{
+    // freopen("input.txt", "r", stdin);
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        string s;
+        cin >> s;
+
+        for (int j = 0; j < M; j++) {
+            char c = s[j];
+            A[i][j] = c;
+
+            if (c == 'J') {
+                if (i==0 || i==N-1 || j==0 || j==M-1) {
+                    cout << 1 << endl;
+                    return 0;
+                }
+                
+                Q1.push({i, j});
+                V1[i][j] = 1;
+                A[i][j] = '.';
+            }
+
+            if (c == 'F') {
+                Q2.push({i, j});
+                V2[i][j] = 1;
+            }
+        }
     }
-  }
-  cout << "IMPOSSIBLE"; // 여기에 도달했다는 것은 탈출에 실패했음을 의미.
+
+    while(!Q2.empty()) {
+        auto cur = Q2.front();
+        Q2.pop();
+
+        for (int dir = 0; dir < 4; dir++) {
+            int x = cur.X + dx[dir];
+            int y = cur.Y + dy[dir];
+
+            if (x<0 || x>= N || y<0 || y>=M)
+                continue;
+            
+            if (A[x][y] == '.' && V2[x][y] == 0) {
+                Q2.push({x, y});
+                V2[x][y] = V2[cur.X][cur.Y] + 1;
+            }
+        }
+    }
+
+    while(!Q1.empty()) {
+        auto cur = Q1.front();
+        Q1.pop();
+
+        for (int dir = 0; dir < 4; dir++) {
+            int x = cur.X + dx[dir];
+            int y = cur.Y + dy[dir];
+
+            if (x<0 || x>=N || y<0 || y>=M)
+                continue;
+            
+            if (A[x][y] == '.' && V1[x][y] == 0)
+                if (V2[x][y] == 0 || V1[cur.X][cur.Y]+1 < V2[x][y]) {
+                    Q1.push({x, y});
+                    V1[x][y] = V1[cur.X][cur.Y] + 1;
+
+                    if (x==0 || x==N-1 || y==0 || y==M-1) {
+                        cout << V1[x][y] << endl;
+                        return 0;
+                    }
+                }
+        }
+    }
+
+    cout << "IMPOSSIBLE" << endl;
 }
