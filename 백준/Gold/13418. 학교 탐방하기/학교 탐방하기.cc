2a50bbb9cf6813bdc3c9c213b6acac6fc5625909
등딱지빌런 <96368476+inhopp@users.noreply.github.com>
2024-11-30 20:56:18 +0900

    [Gold III] Title: 학교 탐방하기, Time: 168 ms, Memory: 11372 KB -BaekjoonHub

---
+++ 백준/Gold/13418. 학교 탐방하기/학교 탐방하기.cc
@@ -0,0 +1,105 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <math.h>
+#include <tuple>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+int P[1002];
+vector<tuple<int, int, int>> V;
+
+int find(int x)
+{
+	while (P[x] > 0)
+		x = P[x];
+
+	return x;
+}
+
+bool uniFind(int u, int v)
+{
+	int r1 = find(u);
+	int r2 = find(v);
+
+	if (r1 == r2)
+		return false;
+
+	if (P[r1] == P[r2]) {
+		P[r1]--;
+		P[r2] = r1;
+	}
+
+	else {
+		if (P[r1] < P[r2])
+			P[r2] = r1;
+
+		else
+			P[r1] = r2;
+	}
+
+	return true;
+}
+
+int main(void)
+{
+	// freopen("input.txt", "r", stdin);
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 0; i <= M; i++) {
+		int x, y, cost;
+		cin >> x >> y >> cost;
+		V.push_back({ cost, x, y });
+	}
+
+	fill(P, P + N + 1, -1);
+	sort(V.rbegin(), V.rend());
+
+	int min_result = 0;
+	int idx = 0, cnt = 0;
+	int cost, x, y;
+
+	while (cnt < N) {
+		tie(cost, x, y) = V[idx++];
+
+		if (!uniFind(x, y))
+			continue;
+
+		min_result += (1 - cost);
+		cnt++;
+	}
+
+	fill(P, P + N + 1, -1);
+	sort(V.begin(), V.end());
+
+	int max_result = 0;
+	idx = 0, cnt = 0;
+
+	while (cnt < N) {
+		tie(cost, x, y) = V[idx++];
+
+		if (!uniFind(x, y))
+			continue;
+
+		max_result += (1 - cost);
+		cnt++;
+	}
+
+	int answer = (max_result * max_result) - (min_result * min_result);
+	cout << answer << endl;
+
+	return 0;
+}
