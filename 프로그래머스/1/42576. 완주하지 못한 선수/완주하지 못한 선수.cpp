58ced54969e4b973433dff3e156f748c2a22dd48
등딱지빌런 <96368476+inhopp@users.noreply.github.com>
2024-12-22 19:27:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.82 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -7,14 +7,13 @@ using namespace std;
 unordered_map<string, int> UM;
 
 string solution(vector<string> participant, vector<string> completion) {
-    for(string p : participant)
-        UM[p] += 1;
+    for(string name : participant)
+        UM[name] += 1;
     
-    for(string c : completion) {
-        if (UM[c] == 1)
-            UM.erase(c);
-        else
-            UM[c] -= 1;
+    for(string name : completion) {
+        UM[name] -= 1;
+        if (UM[name] == 0)
+            UM.erase(name);
     }
     
     string answer = (*UM.begin()).first;
